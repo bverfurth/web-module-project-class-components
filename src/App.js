ae8d5c5c2a749071c8c3ac83858f0812a43dae63
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-08-02 14:32:33 -0400

    installed uuid

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ export default class App extends Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
-  constructor(props) {
+  constructor() {
     super();
-    this.state = {};
+    this.state = {
+      task: "",
+    };
   }
 
   render() {
