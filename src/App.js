72866be10c49f222248142cc9379e28d7f4b302c
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-08-02 15:08:46 -0400

    added item to app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
+import { findAllByPlaceholderText } from "@testing-library/dom";
 import React, { Component } from "react";
+import Todo from "./components/Todo";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+import { v4 as uuidv4 } from "uuid";
+
+const toDoList = [];
 
 export default class App extends Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  //Constructor method for creating an object with a class
   constructor() {
     super();
     this.state = {
-      task: "",
+      toDoList: toDoList,
+      todo: "",
     };
   }
+
+  //Updating State
+
+  //Adding new item to list:
+  addItem = (newItemName) => {
+    this.setState({
+      toDoList: [
+        ...this.state.toDoList,
+        {
+          item: newItemName,
+          id: uuidv4(),
+          completed: false,
+        },
+      ],
+    });
+  };
 
   render() {
     return (
